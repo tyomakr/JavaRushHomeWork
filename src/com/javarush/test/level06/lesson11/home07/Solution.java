40f5e7404c965a403a2e7dc520ac9edf84a0c590
tyomakr <core1024@gmail.com>
2014-01-09 16:39:29 +0200

    work commit

---
+++ src/com/javarush/test/level06/lesson11/home07/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level06.lesson11.home07;
+
+import java.util.ArrayList;
+
+/* Три статические переменных name
+Создай 3 public статических переменных: String Solution.name, String Cat.name, String Dog.name
+*/
+
+public class Solution
+{
+
+    public static String name;
+
+    public static class Cat {
+
+       public static String name;
+    }
+
+    public static class Dog {
+
+        public static String name;
+    }
+}
