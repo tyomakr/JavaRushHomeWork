092294d14a216c643b113aab47ea0a79b58e7c11
tyomakr <core1024@gmail.com>
2014-01-09 14:01:10 +0200

    work commit

---
+++ src/com/javarush/test/level06/lesson11/home01/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level06.lesson11.home01;
+
+/* Класс Cat и статическая переменная catCount
+В классе Cat создай статическую переменную public int catCount.
+Создай конструктор [public Cat()]. Пусть при каждом создании кота (нового объекта Cat) статическая переменная
+catCount увеличивается на 1. Создать 10 объектов Cat и вывести значение переменной catCount на экран.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //Cоздай 10 объектов Cat тут
+
+        // Выведи на экран catCount тут
+    }
+
+    public static class Cat
+    {
+        //Cоздай статическую переменную тут
+
+        //создай конструктор тут
+    }
+
+}
