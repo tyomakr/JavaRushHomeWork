f05696e4f3fdbafad82c570641bc6fbdd88a6c3f
tyomakr <core1024@gmail.com>
2014-01-25 11:35:47 +0200

    work commit

---
+++ src/com/javarush/test/level09/lesson02/task01/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.test.level09.lesson02.task01;
+
+/* Каждый метод должен возвращать свой StackTrace
+Написать пять методов, которые вызывают друг друга. Каждый метод должен возвращать свой StackTrace.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        method1();
+    }
+
+    public static StackTraceElement[] method1()
+    {
+        method2();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element : stackTraceElements) {
+            System.out.println(element.getMethodName());
+        }
+
+        return stackTraceElements;
+    }
+
+    public static StackTraceElement[] method2()
+    {
+        method3();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element : stackTraceElements) {
+            System.out.println(element.getMethodName());
+        }
+
+        return stackTraceElements;
+
+    }
+
+    public static StackTraceElement[] method3()
+    {
+        method4();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element : stackTraceElements) {
+            System.out.println(element.getMethodName());
+        }
+
+        return stackTraceElements;
+
+    }
+
+    public static StackTraceElement[] method4()
+    {
+        method5();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element : stackTraceElements) {
+            System.out.println(element.getMethodName());
+        }
+
+        return stackTraceElements;
+
+    }
+
+    public static StackTraceElement[] method5()
+    {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element : stackTraceElements) {
+            System.out.println(element.getMethodName());
+        }
+
+        return stackTraceElements;
+
+    }
+}
