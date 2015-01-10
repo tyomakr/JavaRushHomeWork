870bca871b419ff46acc798eb336b499dd01ae1a
tyoma.kr <core1024@gmail.com>
2015-01-10 18:28:24 +0300

    home commit

---
+++ src/com/javarush/test/level13/lesson06/task01/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.test.level13.lesson06.task01;
+
+/* Dog, Cat и Mouse
+1. Создать классы Dog, Cat и Mouse.
+2. Реализовать интерфейсы в добавленных классах, учитывая что:
+- Кот(Cat) может передвигаться, может кого-то съесть и может быть сам съеден.
+- Мышь(Mouse) может передвигаться и может быть съедена.
+- Собака(Dog) может передвигаться и съесть кого-то.
+*/
+
+public class Solution
+{
+
+    public interface Moveable //может двигаться
+    {
+        void move();
+    }
+
+    public interface Eatable  //может быть съеден
+    {
+        void eaten();
+    }
+
+    public interface Eat  //может кого-нибудь съесть
+    {
+        void eat();
+    }
+
+    public abstract class Dog implements Moveable, Eat {}
+
+    public abstract class Cat implements Moveable, Eat, Eatable {}
+
+    public abstract class Mouse implements Moveable, Eatable {}
+
+}
