a97b2401e16f7179395afc481bd0dd10f25f8760
tyomakr <core1024@gmail.com>
2015-02-19 16:54:33 +0300

    work commit

---
+++ src/com/javarush/test/level18/lesson03/task03/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level18.lesson03.task03;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/* Самые частые байты
+Ввести с консоли имя файла
+Найти байты, которые чаше всех встречаются в файле
+Вывести их на экран через пробел.
+Закрыть поток ввода-вывода
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        FileInputStream iStream = new FileInputStream(fileName);
+
+        HashMap <String, Integer> map = new HashMap<>();
+
+
+
+
+    }
+}
