ca9ad4e526293c577e32a2db7f315676ce854c58
tyomakr <core1024@gmail.com>
2015-08-05 08:45:36 +0300

    work commit

---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,4 @@
+package com.javarush.test.level24.lesson02.home01;
+
+public interface SelfInterfaceMarker {
+}
---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level24.lesson02.home01;
+
+
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+    public SelfInterfaceMarkerImpl() {
+    }
+
+
+    public void consoleMethod() {
+
+    }
+
+    public boolean bool() {
+        return true;
+    }
+
+}
