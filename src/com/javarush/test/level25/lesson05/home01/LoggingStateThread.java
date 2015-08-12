3a0a2f42be0d6bc9ad5f480dc864b386303953eb
tyomakr <core1024@gmail.com>
2015-08-12 17:51:15 +0300

    work commit

---
+++ src/com/javarush/test/level25/lesson05/home01/LoggingStateThread.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level25.lesson05.home01;
+
+
+public class LoggingStateThread extends Thread
+{
+    Thread target;
+    public LoggingStateThread(Thread target)
+    {
+        this.target = target;
+        setDaemon(true);
+
+    }
+
+    @Override
+    public void run()
+    {
+        State state = this.target.getState();
+        System.out.println(state);
+        while (state != State.TERMINATED)
+        {
+            if (state != this.target.getState())
+            {
+                state = this.target.getState();
+                System.out.println(state);
+            }
+        }
+    }
+}
