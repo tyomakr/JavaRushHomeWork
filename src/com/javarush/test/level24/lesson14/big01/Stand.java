33d08b95d7040060e4b140aa77b2713c4990d02e
tyomakr <core1024@gmail.com>
2015-08-12 10:19:34 +0300

    work commit

--- src/com/javarush/test/level24/lesson14/big01/Stand.java
+++ src/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -1,19 +1,72 @@
 package com.javarush.test.level24.lesson14.big01;
 
-public class Stand extends BaseObject {
+/**
+ *  Подставка, с помощью которой отражаем мячик.
+ */
+public class Stand extends BaseObject
+{
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
 
+    //скорость
+    private double speed = 1;
+    //направление (-1 влево, +1 вправо)
+    private double direction = 0;
 
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    public Stand(double x, double y)
+    {
+        super(x,y,3);
     }
 
-    @Override
-    public void draw(Canvas canvas) {
+    /**
+     * Метод передвигает подставку в соответствии с текущим значением direction.
+     */
+    public void move()
+    {
+        double dx = speed * direction;
+        x = x + dx;
 
+        checkBorders(radius, Arcanoid.game.getWidth() - radius + 1, 1, Arcanoid.game.getHeight() + 1);
     }
 
-    @Override
-    public void move() {
+    /**
+     * direction устанавливается равным -1
+     */
+    public void moveLeft()
+    {
+        direction = -1;
+    }
 
+    /**
+     * direction устанавливается равным +1
+     */
+    public void moveRight()
+    {
+        direction = 1;
+    }
+
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    public double getDirection()
+    {
+        return direction;
+    }
+
+    /**
+     * Отрисовываем себя на холсте
+     */
+    @Override
+    public void draw(Canvas canvas)
+    {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
     }
 }
