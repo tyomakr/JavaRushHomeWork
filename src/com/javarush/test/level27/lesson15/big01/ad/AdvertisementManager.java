862d8e4b3d247b4a76e8a9d17e75d06a03f5daa1
tyomakr <core1024@gmail.com>
2015-09-11 13:43:46 +0300

    work commit

--- src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
+++ src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
@@ -2,5 +2,5 @@ package com.javarush.test.level27.lesson15.big01.ad;
 
 public class AdvertisementManager {
 
-
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 }
--- src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementStorage.java
+++ src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementStorage.java
@@ -1,13 +1,35 @@
 package com.javarush.test.level27.lesson15.big01.ad;
 
 
-public class AdvertisementStorage {
-    private static AdvertisementStorage ourInstance = new AdvertisementStorage();
+import java.util.ArrayList;
+import java.util.List;
 
+class AdvertisementStorage {
+
+    private static AdvertisementStorage ourInstance = new AdvertisementStorage();
     public static AdvertisementStorage getInstance() {
         return ourInstance;
     }
 
+    private final List<Advertisement> videos = new ArrayList<>();
+
     private AdvertisementStorage() {
+
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));  //10 min
     }
+
+
+
+    public List list() {
+        return videos;
+    }
+
+    public void add (Advertisement advertisement) {
+
+        videos.add(advertisement);
+    }
+
 }
