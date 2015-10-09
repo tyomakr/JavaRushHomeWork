38ee59a73fa065e101264607277b4e95a0f7fed0
tyomakr <core1024@gmail.com>
2015-10-09 14:33:25 +0300

    work commit

--- src/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/com/javarush/test/level30/lesson15/big01/Server.java
@@ -1,7 +1,117 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Server {
 
+    // ключ -имя клиента, а значение - соединение с ним
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
+
+    /**обработчик Handler, в котором будет происходить обмен сообщениями с клиентом **/
+    private static class Handler extends Thread {
+
+        private Socket socket;
+
+        //Constructor
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        /** Handshake **/
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+
+            while (true) {
+                // Сформировать и отправить команду запроса имени пользователя
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                // Получить ответ клиента
+                Message message = connection.receive();
+
+                // Проверить, что получена команда с именем пользователя
+                if (message.getType() == MessageType.USER_NAME) {
+
+                    //Достать из ответа имя, проверить, что оно не пустое
+                    if (message.getData() != null && !message.getData().isEmpty()) {
+
+                        // и пользователь с таким именем еще не подключен (используй connectionMap)
+                        if (connectionMap.get(message.getData()) == null) {
+
+                            // Добавить нового пользователя и соединение с ним в connectionMap
+                            connectionMap.put(message.getData(), connection);
+                            // Отправить клиенту команду информирующую, что его имя принято
+                            connection.send(new Message(MessageType.NAME_ACCEPTED));
+
+                            // Вернуть принятое имя в качестве возвращаемого значения
+                            return message.getData();
+                        }
+                    }
+                }
+            }
+        }
+
+        /** Отправка списка всех пользователей **/
+        private void sendListOfUsers(Connection connection, String userName) throws IOException {
+
+            for (String key : connectionMap.keySet()) {
+                Message message = new Message(MessageType.USER_ADDED, key);
+
+                if (!key.equals(userName)) {
+                    connection.send(message);
+                }
+            }
+        }
+
+    }
+
+
+    /** отправка сообщения для всех **/
+    public static void sendBroadcastMessage(Message message) {
+
+        try {
+
+            for (Connection connection : connectionMap.values()) {
+                connection.send(message);
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+            ConsoleHelper.writeMessage("Сообщение не отправлено");
+        }
+
+    }
+
+
+    /** MAIN **/
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = null;
+
+        try {
+            // вводим порт
+            serverSocket = new ServerSocket(ConsoleHelper.readInt());
+            ConsoleHelper.writeMessage("Сервер запущен");
+
+
+            while (true) {
+                //Слушаем
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                //запускаем handler
+                handler.start();
+            }
+
+        } catch (Exception e) {
+
+            serverSocket.close();
+            e.printStackTrace();
+        }
+
+    }
 
 }
+
+
