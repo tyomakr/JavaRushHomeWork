02426f9b4a1756f42ec2c70862299730443922e7
tyomakr <core1024@gmail.com>
2015-10-13 15:21:05 +0300

    work commit

--- src/com/javarush/test/level30/lesson15/big01/client/Client.java
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -1,14 +1,73 @@
 package com.javarush.test.level30.lesson15.big01.client;
 
 import com.javarush.test.level30.lesson15.big01.Connection;
+import com.javarush.test.level30.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level30.lesson15.big01.Message;
+import com.javarush.test.level30.lesson15.big01.MessageType;
+
+import java.io.IOException;
 
 public class Client {
 
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
+
+    /** Methods **/
+    /** Должен запросить ввод адреса сервера и вернуть введенное значение**/
+    protected String getServerAddress() {
+
+        ConsoleHelper.writeMessage("Введите адрес сервера: ");
+        return ConsoleHelper.readString();
+    }
+
+
+    /** Должен запрашивать ввод порта сервера и возвращать его **/
+    protected int getServerPort() {
+
+        ConsoleHelper.writeMessage("Введите порт сервера: ");
+        return ConsoleHelper.readInt();
+    }
+
+
+    /** Должен запрашивать и возвращать имя пользователя **/
+    protected String getUserName() {
+
+        ConsoleHelper.writeMessage("Введите имя пользователя: ");
+        return ConsoleHelper.readString();
+    }
+
+
+    protected boolean shouldSentTextFromConsole() {
+
+        return true;
+    }
+
+
+    /** должен создавать и возвращать новый объект класса SocketThread **/
+    protected SocketThread getSocketThread() {
+
+        return new SocketThread();
+    }
+
+
+    /**  создает новое текстовое сообщение, используя переданный текст и отправляет его серверу через соединение connection **/
+    protected void sendTextMessage(String text) {
+
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Ошибка отправки");
+            clientConnected = false;
+        }
+
+    }
+
+
     /** SocketThread **/
     public class SocketThread extends Thread {
 
     }
+
 }
