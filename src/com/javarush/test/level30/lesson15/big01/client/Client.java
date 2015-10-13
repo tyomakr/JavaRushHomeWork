b70af4d126d783e9394eadd3e9c9298836cb2fde
tyomakr <core1024@gmail.com>
2015-10-13 14:51:22 +0300

    work commit

---
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level30.lesson15.big01.client;
+
+import com.javarush.test.level30.lesson15.big01.Connection;
+
+public class Client {
+
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    /** SocketThread **/
+    public class SocketThread extends Thread {
+
+    }
+}
