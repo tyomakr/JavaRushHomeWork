d9a80a9248a3cf2f2613ce6c2c35c8921f8b47d9
tyomakr <core1024@gmail.com>
2015-10-14 16:32:44 +0300

    work commit

--- src/com/javarush/test/level30/lesson15/big01/client/BotClient.java
+++ src/com/javarush/test/level30/lesson15/big01/client/BotClient.java
@@ -1,5 +1,11 @@
 package com.javarush.test.level30.lesson15.big01.client;
 
+import com.javarush.test.level30.lesson15.big01.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class BotClient extends Client {
 
     // Bots counter
@@ -14,6 +20,72 @@ public class BotClient extends Client {
 
     /** inner class */
     public class BotSocketThread extends SocketThread {
+
+
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+
+            //	С помощью метода sendTextMessage() отправь сообщение с текстом
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+
+            // Вызови реализацию clientMainLoop() родительского класса
+            super.clientMainLoop();
+
+        }
+
+
+        @Override
+        protected void processIncomingMessage(String message) {
+
+            // Вывести в консоль текст полученного сообщения message
+            ConsoleHelper.writeMessage(message);
+
+            // Получить из message имя отправителя и текст сообщения. Они разделены ": "
+            String senderName = "";
+            String senderMessageText;
+
+            if (message.contains(": ")) {
+                senderName = message.substring(0, message.indexOf(": "));
+                senderMessageText = message.substring(message.indexOf(": ") + 2);
+            }
+            else {
+                senderMessageText = message;
+            }
+
+
+            SimpleDateFormat format = null;
+            // Отправить ответ в зависимости от текста принятого сообщения. Если текст сообщения:
+            if ("дата".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("d.MM.YYYY");
+            }
+            else if ("день".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("d");
+            }
+            else if ("месяц".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("MMMM");
+            }
+            else if ("год".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("YYYY");
+            }
+            else if ("время".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("H:mm:ss");
+            }
+            else if ("час".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("H");
+            }
+            else if ("минуты".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("m");
+            }
+            else if ("секунды".equalsIgnoreCase(senderMessageText)) {
+                format = new SimpleDateFormat("s");
+            }
+
+            if (format != null)
+            {
+                sendTextMessage("Информация для " + senderName + ": " + format.format(Calendar.getInstance().getTime()));
+            }
+
+        }
 
     }
 
