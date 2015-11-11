be320d3d2ab664179c2762f46be180b496b79d64
tyomakr <core1024@gmail.com>
2015-11-11 16:41:20 +0300

    work commit

--- src/com/javarush/test/level29/lesson15/big01/user/UserHelper.java
+++ src/com/javarush/test/level29/lesson15/big01/user/UserHelper.java
@@ -7,43 +7,27 @@ public class UserHelper {
     private boolean isManAnya = false;
     private boolean isManRoma = true;
 
+
     public void printUsers() {
-        System.out.println("Имя: " + userAnya.getName());
-        System.out.println("Фамилия: " + userAnya.getSurname());
-        printAdditionalInfo(userAnya);
-
-        System.out.println("Имя: " + userRoma.getName());
-        System.out.println("Фамилия: " + userRoma.getSurname());
-        printAdditionalInfo(userRoma);
+        userAnya.printInfo();
+        userRoma.printInfo();
     }
 
-    public void printAdditionalInfo(User user) {
-        if (ageLessThan16(user))
-            System.out.println("Пользователь моложе 16 лет");
-        else
-            System.out.println("Пользователь старше 16 лет");
-    }
 
-    private boolean ageLessThan16(User user) {
-        if (user.getAge() < 16) {
-            return true;
-        }
-        return false;
-    }
 
     public int calculateAvarageAge() {
-        int age = 28;
-        User userUra = new User("Юра", "Карп", age);
 
-        age = (userAnya.getAge() + userRoma.getAge() + userUra.getAge()) / 3;
 
-        return age;
+        User userUra = new User("Юра", "Карп", 28);
+
+        return (userAnya.getAge() + userRoma.getAge() + userUra.getAge()) / 3;
     }
 
-    public void calculateRate(double base, int age, boolean hasWork, boolean hasHouse) {
+    public double calculateRate(double base, int age, boolean hasWork, boolean hasHouse) {
         base += age / 100;
         base *= hasWork ? 1.1 : 0.9;
         base *= hasHouse ? 1.1 : 0.9;
+        return base;
     }
 
     public String getBoosName(User user) {
