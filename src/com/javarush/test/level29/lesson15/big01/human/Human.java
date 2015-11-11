d2e7b6ce84ae153b58e9eb3733b2797f8230ff15
tyomakr <core1024@gmail.com>
2015-11-11 15:07:37 +0300

    work commit

--- src/com/javarush/test/level29/lesson15/big01/human/Human.java
+++ src/com/javarush/test/level29/lesson15/big01/human/Human.java
@@ -97,32 +97,5 @@ public class Human implements Alive {
         public int weight;
     }
 
-
-    public static class BloodGroup {
-
-        private final int code;
-
-        private BloodGroup(int code) {
-            this.code = code;
-        }
-
-
-        public int getCode() {
-            return code;
-        }
-        public static BloodGroup first() {
-            return new BloodGroup(1);
-        }
-        public static BloodGroup second() {
-            return new BloodGroup(2);
-        }
-        public static BloodGroup third() {
-            return new BloodGroup(3);
-        }
-        public static BloodGroup fourth() {
-            return new BloodGroup(4);
-        }
-
-    }
-
 }
+
