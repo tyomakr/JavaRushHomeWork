b106ba7b4a010b26ff0ecba050f2c07de6ca51ec
tyomakr <core1024@gmail.com>
2015-11-18 14:24:34 +0300

    work commit

---
+++ src/com/javarush/test/level27/lesson15/big01/statistic/event/CookedOrderEventDataRow.java
@@ -0,0 +1,47 @@
+package com.javarush.test.level27.lesson15.big01.statistic.event;
+
+import com.javarush.test.level27.lesson15.big01.kitchen.Dish;
+
+import java.util.Date;
+import java.util.List;
+
+public class CookedOrderEventDataRow implements EventDataRow
+{
+    private String tabletName;
+    private String cookName;
+    private int cookingTimeSeconds;
+    private List<Dish> cookingDishs;
+    private Date currentDate;
+
+    public CookedOrderEventDataRow(String tabletName, String cookName, int cookingTimeSeconds, List<Dish> cookingDishs)
+    {
+        this.tabletName = tabletName.toString();
+        this.cookName = cookName;
+        this.cookingTimeSeconds = cookingTimeSeconds;
+        this.cookingDishs = cookingDishs;
+        this.currentDate = new Date();
+    }
+
+    public String getCookName()
+    {
+        return cookName;
+    }
+
+    @Override
+    public EventType getType()
+    {
+        return EventType.COOKED_ORDER;
+    }
+
+    @Override
+    public Date getDate()
+    {
+        return currentDate;
+    }
+
+    @Override
+    public int getTime()
+    {
+        return cookingTimeSeconds;
+    }
+}
--- src/com/javarush/test/level27/lesson15/big01/statistic/event/EventDataRow.java
+++ src/com/javarush/test/level27/lesson15/big01/statistic/event/EventDataRow.java
@@ -1,4 +1,12 @@
 package com.javarush.test.level27.lesson15.big01.statistic.event;
 
-public interface EventDataRow {
+import java.util.Date;
+
+public interface EventDataRow
+{
+    EventType getType();
+
+    Date getDate();
+
+    int getTime();
 }
