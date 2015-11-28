ed0fdce346dcee18b1dd9403de41d277b379f5c4
tyomakr <tyoma.kr@gmail.com>
2015-11-28 19:44:20 +0300

    hc

--- src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.test.level31.lesson15.big01;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
 
@@ -11,6 +15,25 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
 
+
     //methods
-    public void createZip(Path source) throws Exception {}
+    public void createZip(Path source) throws Exception {
+
+        try(ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+            InputStream inputStream = Files.newInputStream(source)) {
+
+            ZipEntry sourceEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(sourceEntry);
+
+            //Считываем содержимое файла в массив byte
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+
+            //Добавляем содержимое к архиву
+            zipOutputStream.write(buffer);
+            //Закрываем текущую запись для новой записи
+            zipOutputStream.closeEntry();
+
+        }
+    }
 }
