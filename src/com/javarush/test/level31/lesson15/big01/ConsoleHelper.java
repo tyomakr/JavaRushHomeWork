d66c63ccefeaae7a0e81a05a2125e41e0c142b82
tyomakr <tyoma.kr@gmail.com>
2015-11-28 21:21:52 +0300

    hc

--- src/com/javarush/test/level31/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level31/lesson15/big01/ConsoleHelper.java
@@ -4,24 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-
 public class ConsoleHelper {
-
-
-    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
     public static void writeMessage(String message) {
         System.out.println(message);
     }
 
-
     public static String readString() throws IOException {
+        String text = bis.readLine();
+        return text;
+    }
 
-        String message;
-        message = reader.readLine();
-
-        return message;
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text.trim());
     }
 }
