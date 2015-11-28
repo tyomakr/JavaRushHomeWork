59cb519da63567441d4d5b28f6f3805e7f03992e
tyomakr <tyoma.kr@gmail.com>
2015-11-28 21:59:04 +0300

    hc

--- src/com/javarush/test/level31/lesson15/big01/Archiver.java
+++ src/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -1,16 +1,29 @@
 package com.javarush.test.level31.lesson15.big01;
 
+import com.javarush.test.level31.lesson15.big01.command.ExitCommand;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        ConsoleHelper.writeMessage("Введите полный путь к архиву: ");
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(ConsoleHelper.readString()));
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Введите полный путь к архиву: ");
+            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(reader.readLine()));
 
-        ConsoleHelper.writeMessage("Введите путь к архивируемому файлу: ");
-        zipFileManager.createZip(Paths.get(ConsoleHelper.readString()));
+            System.out.println("Введите путь к архивируемому файлу: ");
+            zipFileManager.createZip(Paths.get(reader.readLine()));
 
+            reader.close();
+
+            ExitCommand exitCommand = new ExitCommand();
+            exitCommand.execute();
+
+
+        } catch (Exception e) {}
     }
 }
