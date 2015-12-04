ecab6e1144914d45ed127e5b4fd4b013b57f7ea3
tyomakr <core1024@gmail.com>
2015-12-04 14:27:54 +0300

    work commit

--- src/com/javarush/test/level31/lesson15/big01/command/ZipExtractCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipExtractCommand.java
@@ -1,10 +1,31 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipExtractCommand extends ZipCommand {
 
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
 
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path outputPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(outputPath);
+            ConsoleHelper.writeMessage("Архив распакован.");
+
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
+
 }
