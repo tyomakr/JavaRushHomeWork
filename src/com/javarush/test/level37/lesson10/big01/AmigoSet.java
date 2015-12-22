7a8e5369e3b60d3df79efa8e4c28008176fbed25
tyomakr <core1024@gmail.com>
2015-12-22 17:32:35 +0300

    work commit

---
+++ src/com/javarush/test/level37/lesson10/big01/AmigoSet.java
@@ -0,0 +1,119 @@
+package com.javarush.test.level37.lesson10.big01;
+
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.*;
+
+
+public class AmigoSet<E> extends AbstractSet<E> implements Cloneable, Serializable, Set<E> {
+
+    private final static Object PRESENT = new Object();
+    private transient HashMap<E,Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection){
+        map = new HashMap<>((int)Math.max(16,collection.size()/.75f));
+        this.addAll(collection);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return null == map.put(e,PRESENT);
+    }
+
+
+    @Override
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return map.keySet().size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.keySet().isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.keySet().contains(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.keySet().remove(o);
+    }
+
+    @Override
+    public Object clone() {
+        AmigoSet<E> amigoSet = new AmigoSet<>();
+
+        try {
+            amigoSet.addAll(this);
+            amigoSet.map.putAll(this.map);
+        }
+        catch (Exception e){
+            throw new InternalError();
+        }
+
+        return amigoSet;
+    }
+
+
+    private void writeObject(java.io.ObjectOutputStream s) throws IOException {
+
+        s.defaultWriteObject();
+
+        s.writeObject(map.size());
+        for(E e:map.keySet()){
+            s.writeObject(e);
+        }
+        s.writeObject(HashMapReflectionHelper.callHiddenMethod(map,"capacity"));
+        s.writeObject(HashMapReflectionHelper.callHiddenMethod(map,"loadFactor"));
+    }
+
+    private void readObject(java.io.ObjectInputStream s) throws IOException, ClassNotFoundException {
+        s.defaultReadObject();
+
+        int size = (int)s.readObject();
+        Set<E> set = new HashSet<>();
+        for(int i =0;i<size;i++){
+            set.add((E)s.readObject());
+        }
+        int capacity = (int)s.readObject();
+        float loadFactor = (float)s.readObject();
+        map = new HashMap<>(capacity,loadFactor);
+        for(E e:set){
+            map.put(e,PRESENT);
+        }
+    }
+
+
+}
+
+
+/*
+Задание 5
+
+Твое собственное множество AmigoSet реализует интерфейс Serializable. Однако, не сериализуется правильно.
+
+1. Реализуй свою логику сериализации и десериализации.
+Вспоминай, какие именно приватные методы нужно добавить, чтоб сериализация пошла по твоему сценарию.
+Для сериализации:
+* сериализуй сет
+* сериализуй capacity и loadFactor у объекта map, они понадобятся для десериализации.
+ Т.к. эти данные ограничены пакетом, то воспользуйся утилитным классом HashMapReflectionHelper, чтобы достать их.
+
+Для десериализации:
+* вычитай все данные
+* создай мапу используя конструктор с capacity и loadFactor
+
+2. Помнишь, что такое transient?
+ */
