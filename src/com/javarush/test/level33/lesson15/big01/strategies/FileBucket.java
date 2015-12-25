17d8f83854203d04f3fab152ce121e0615868c3c
tyomakr <core1024@gmail.com>
2015-12-25 11:54:10 +0300

    work commit

--- src/com/javarush/test/level33/lesson15/big01/strategies/FileBucket.java
+++ src/com/javarush/test/level33/lesson15/big01/strategies/FileBucket.java
@@ -1,12 +1,10 @@
 package com.javarush.test.level33.lesson15.big01.strategies;
 
-import com.javarush.test.level33.lesson15.big01.ExceptionHandler;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class FileBucket implements Serializable {
+public class FileBucket {
 
 
     private Path path;
@@ -15,11 +13,55 @@ public class FileBucket implements Serializable {
         try {
             path = Files.createTempFile("tmp", null, null);
         }
-        catch (IOException e) {
-            ExceptionHandler.log(e);
-        }
+        catch (IOException e) {}
         path.toFile().deleteOnExit();
     }
+
+    //он должен возвращать размер файла на который указывает path
+    public long getFileSize() {
+        return path.toFile().length();
+    }
+
+    //должен сериализовывать переданный entry в файл. Учти, каждый entry может содержать еще один entry
+    public void putEntry(Entry entry) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(path))) {
+            Entry e = entry;
+            while (e != null) {
+                objectOutputStream.writeObject(e);
+                e = e.next;
+            }
+        }
+        catch (IOException e) {}
+    }
+
+
+
+    //должен забирать entry из файла. Если файл имеет нулевой размер, вернуть null
+    public Entry getEntry() {
+        if (getFileSize() == 0) return null;
+
+        try (ObjectInputStream s = new ObjectInputStream(Files.newInputStream(path))) {
+            Entry result = (Entry) s.readObject();
+            Entry last = result;
+            Entry temp;
+            while ((temp = (Entry)s.readObject()) != null) {
+                last.next = temp;
+                last = temp;
+            }
+            return result;
+        }
+        catch (IOException | ClassNotFoundException e) {
+            return null;
+        }
+
+    }
+    //удалять файл на который указывает path
+    public void remove() {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {}
+    }
+
 
 
 
@@ -49,6 +91,6 @@ deleteOnExit().
 файл. Учти, каждый entry может содержать еще один entry.
 9.4.3.	Entry getEntry() - должен забирать entry из файла. Если файл имеет нулевой
 размер, вернуть null.
-9.4.4.	void remove() – удалять файл на который указывает path.
+9.4.4.	void remove() – далять файл на который указывает path.
 Конструктор и методы не должны кидать исключения.
  */
