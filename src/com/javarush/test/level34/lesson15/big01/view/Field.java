41e0750476e86092977089ca823453c4ed729bc2
tyomakr <core1024@gmail.com>
2016-02-02 14:46:46 +0300

    work commit

--- src/com/javarush/test/level34/lesson15/big01/view/Field.java
+++ src/com/javarush/test/level34/lesson15/big01/view/Field.java
@@ -6,6 +6,8 @@ import com.javarush.test.level34.lesson15.big01.model.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Field extends JPanel {
 
@@ -14,6 +16,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler(this);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void paint(Graphics g) {
@@ -27,8 +32,35 @@ public class Field extends JPanel {
 
     }
 
-
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
+
+
+    public static class KeyHandler extends KeyAdapter {
+
+        private Field field;
+
+        public KeyHandler(Field field) {
+            this.field = field;
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            switch (e.getKeyCode()) {
+                case (KeyEvent.VK_LEFT):
+                    field.eventListener.move(Direction.LEFT);
+                case (KeyEvent.VK_RIGHT):
+                    field.eventListener.move(Direction.RIGHT);
+                case (KeyEvent.VK_UP):
+                    field.eventListener.move(Direction.UP);
+                case (KeyEvent.VK_DOWN):
+                    field.eventListener.move(Direction.DOWN);
+                case (KeyEvent.VK_R):
+                    field.eventListener.restart();
+            }
+        }
+    }
+
 }
